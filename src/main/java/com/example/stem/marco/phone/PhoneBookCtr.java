4e8d08973523fa452b24ed884b61ae667299a056
marco <marco@ACER-DI-MARCO.homenet.telecomitalia.it>
2022-12-14 00:59:07 +0100

    error fixed

---
+++ src/main/java/com/example/stem/marco/phone/PhoneBookCtr.java
@@ -0,0 +1,39 @@
+package com.example.stem.marco.phone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.stem.dao.Contact;
+
+@Controller
+@RequestMapping("/marco/phonebook")
+public class PhoneBookCtr {
+	private PhoneBookSrv srv;
+	
+	private static Logger log = LoggerFactory.getLogger(PhoneBookCtr.class);
+	
+	public PhoneBookCtr(PhoneBookSrv srv) {
+		this.srv = srv;
+	}
+	
+	@GetMapping("/insert")
+	public String insert(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String phone, Model model) {
+		log.trace("enter insert()");
+		Contact contact = new Contact(firstName, lastName, phone);
+		srv.newAdd(contact);
+		model.addAttribute("contacts", srv.getAll());
+		return "/marco/phonebook/phonebook";
+	}
+	
+	@GetMapping
+	public String home(Model model) {
+		log.trace("enter home");
+  	model.addAttribute("contacts", srv.getAll());
+		return "/marco/phonebook/phonebook";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/marco/phonebook/phonebook.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Phonebook</title>
+</head>
+<body>
+	<h1>Phonebook</h1>
+	<table> 
+		<tr>
+			<th>First name |</th>
+			<th> Last name |</th>
+			<th> Phone</th>
+	</table>
+	<table>
+		<c:forEach var="contact" items="${contacts}">
+			<tr>
+				<td>${contact.firstName}</td>
+				<td>${contact.lastName}</td>
+				<td>${contact.phone}</td>
+			</tr>
+		</c:forEach>
+	</table>
+	<h3>Contact to add</h3>
+	<form method="post" action="insert">
+		<input type="text" name="firstName" placeholder="First name"> 
+		<input type="text" name="lastName" placeholder="Last name"> 
+		<input type="text" name="phone" placeholder="Phone"> 
+		<input type="submit" value="Insert"/>
+	</form>
+	<h3>Contact to remove</h3>
+	<form method="post" action="remove">
+		<input type="text" name="phone" placeholder="Phone"> 
+		<input type="submit" value="Remove"/>
+	</form>
+</body>
+</html>
