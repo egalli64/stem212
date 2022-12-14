1c270d9257fd5e8425889e2790ac0ff96c99080a
marco <marco@ACER-DI-MARCO.homenet.telecomitalia.it>
2022-12-14 15:01:01 +0100

    index fixed

--- src/main/java/com/example/stem/marco/phone/PhoneBookCtr.java
+++ src/main/java/com/example/stem/marco/phone/PhoneBookCtr.java
@@ -1,5 +1,7 @@
 package com.example.stem.marco.phone;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,36 +13,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.stem.dao.Contact;
 
 @Controller
-@RequestMapping("/marco/phonebook")
+@RequestMapping("/marco/phone")
 public class PhoneBookCtr {
-	private PhoneBookSrv srv;
 	
 	private static Logger log = LoggerFactory.getLogger(PhoneBookCtr.class);
 	
-	public PhoneBookCtr(PhoneBookSrv srv) {
-		this.srv = srv;
+	private PhoneBookRep repo;
+
+	public PhoneBookCtr(PhoneBookRep repo) {
+		this.repo = repo;
 	}
+	
+//	private PhoneBookSrv srv;
+//	
+//	public PhoneBookCtr(PhoneBookSrv srv) {
+//		this.srv = srv;
+//	}
+	
+//	@GetMapping("/insert")
+//	public String insert(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String phone, Model model) {
+//		log.trace("enter insert()");
+//		Contact contact = new Contact(firstName, lastName, phone);
+//		srv.newAdd(contact);
+//		model.addAttribute("contacts", srv.getAll());
+//		return "/marco/phonebook/phonebook";
+//	}
 	
 	@GetMapping("/insert")
 	public String insert(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String phone, Model model) {
 		log.trace("enter insert()");
 		Contact contact = new Contact(firstName, lastName, phone);
-		srv.newAdd(contact);
-		model.addAttribute("contacts", srv.getAll());
+		repo.save(contact);
+		model.addAttribute("contacts", repo.findAll());
 		return "/marco/phonebook/phonebook";
 	}
 	
-	@GetMapping()
-	public String home(Model model) {
-		log.trace("enter home");
-  	model.addAttribute("contacts", srv.getAll());
-		return "/marco/phonebook/phonebook";
-	}
-	
-//	@GetMapping("/remove")
-//	public String remove(@RequestParam String phone, Model model) {
-//		log.trace("enter remove()");
-//		srv.remove(conatct);
-//		reutrn"marco/phonebook/removed";
+//	@GetMapping()
+//	public String home(Model model) {
+//		log.trace("enter home");
+//  	model.addAttribute("contacts", srv.getAll());
+//		return "/marco/phonebook/phonebook";
 //	}
+	
+	@GetMapping
+	public String home(Model model) {
+		model.addAttribute("contacts", repo.findAll());
+		return "/marco/phonebook/phonebook";
+	}
+	
+	@GetMapping("/remove")
+	public String remove(@RequestParam Integer id, Model model) {
+		log.trace("remove contact");
+		Optional<Contact> contact = repo.findById(id);
+		contact.ifPresent(c -> repo.delete(c));
+		model.addAttribute("contacts", repo.findAll());
+		return "/marco/phonebook/phonebook";
+	}
 }
--- src/main/webapp/WEB-INF/jsp/marco/phonebook/phonebook.jsp
+++ src/main/webapp/WEB-INF/jsp/marco/phonebook/phonebook.jsp
@@ -4,12 +4,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="/marco/css/phonebookstyle.css">
 <title>Phonebook</title>
 </head>
 <body>
 	<h1>Phonebook</h1>
 	<table> 
 		<tr id="first-row">
+			<th> ID |</th>
 			<th>First name |</th>
 			<th> Last name |</th>
 			<th> Phone</th>
@@ -17,6 +19,7 @@
 	<table>
 		<c:forEach var="contact" items="${contacts}">
 			<tr>
+				<td>${contact.id}</td>
 				<td>${contact.firstName}</td>
 				<td>${contact.lastName}</td>
 				<td>${contact.phone}</td>
@@ -28,15 +31,15 @@
 		<p id="error">${error}</p>
 	</c:if>
 	
-	<form action="/marco/phonebook/insert">
+	<form action="/marco/phone/insert">
 		<input type="text" name="firstName" placeholder="First name"> 
 		<input type="text" name="lastName" placeholder="Last name"> 
 		<input type="text" name="phone" placeholder="Phone"> 
 		<input type="submit" value="Insert"/>
 	</form>
 	<h3>Contact to remove</h3>
-	<form action="/marco/phonebook/remove">
-		<input type="text" name="phone" placeholder="Phone"> 
+	<form action="/marco/phone/remove">
+		<input type="text" name="id" placeholder="ID"> 
 		<input type="submit" value="Remove"/>
 	</form>
 </body>
