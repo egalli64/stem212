4e0c496abd19362828c8e3ed30dac97b5cf10b47
caloro <caloro@Isabella>
2022-12-14 12:40:33 +0100

    Modifiche repo

--- src/main/java/com/example/stem/isabella/phone/IsabellaPhoneCtr.java
+++ src/main/java/com/example/stem/isabella/phone/IsabellaPhoneCtr.java
@@ -1,5 +1,7 @@
 package com.example.stem.isabella.phone;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,33 +10,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.stem.dao.Contact;
+
 @Controller
 @RequestMapping("/isabella/phone")
 public class IsabellaPhoneCtr {
 	private static Logger log = LoggerFactory.getLogger(IsabellaPhoneCtr.class);
-	private IsabellaPhoneSvc svc;
+	private PhoneRepo repo;
 
-	public IsabellaPhoneCtr(IsabellaPhoneSvc svc) {
-		this.svc = svc;
+	public IsabellaPhoneCtr(PhoneRepo repo) {
+		this.repo = repo;
 	}
+
 	@GetMapping("/delete")
-	public String delete() {
-		log.trace("delete contact");	
+	public String delete(@RequestParam Integer id, Model model) {
+		log.trace("delete contact");
+		Optional<Contact> contact = repo.findById(id);
+		contact.ifPresent(c -> repo.delete(c));
+		model.addAttribute("contacts", repo.findAll());
 		return "/isabella/phonebook";
 	}
 
 	@GetMapping("/insert")
-	public String insert(@RequestParam String name, @RequestParam String lastname, @RequestParam String number, Model model) {
+	public String insert(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String phone,
+			Model model) {
 		log.trace("enter insert()");
-		IsabellaContact contact = new IsabellaContact(name, lastname, number);
-		svc.add(contact);
-		model.addAttribute("contacts", svc.getAll());
+		Contact contact = new Contact(firstName, lastName, phone);
+		repo.save(contact);
+		model.addAttribute("contacts", repo.findAll());
 		return "/isabella/phonebook";
 	}
 
 	@GetMapping
 	public String home(Model model) {
-		model.addAttribute("contacts", svc.getAll());
+		model.addAttribute("contacts", repo.findAll());
 		return "/isabella/phonebook";
 	}
 }
